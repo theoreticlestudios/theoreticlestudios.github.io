f5506adcdd05cbfb77beac157132d6d1e02011f6
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2021-09-28 18:28:23 -0500

    added deploy options

--- theoreticlewebsite/src/main.js
+++ theoreticlewebsite/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(router).use(Quasar).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).use(router).use(Quasar, quasarUserOptions).mount('#app')
--- theoreticlewebsite/src/views/Home.vue
+++ theoreticlewebsite/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
+   <div class="row" style="height: 100vh">
+    <div class="col-3" id="navbar-col">
+      <p class="text-h2 abril text-white" style="text-align: left; padding-top: 4vh; padding-left: 2vw">Theoreticle<br>Studios</p>
 
+    </div>
+    <div class="col-9">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,14 +14,15 @@ export default {
   name: 'Home',
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
+#navbar-col {
+  background-image: linear-gradient(rgba(53, 0, 211, 0.7), rgba(53, 0, 211, 0.5)), url("../assets/90-deg-rotatedgigabyte.png");
+  background-repeat: no-repeat, no-repeat;
+  background-size: cover, cover;
 }
+.abril {
+  font-family: "Abril Fatface";
+}
+
 </style>
