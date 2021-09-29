ace66d672fc59516a27c45f5bf823d4fdf840c55
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2021-09-28 19:05:54 -0500

    Delete theoreticlewebsite directory

--- theoreticlewebsite/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-
-createApp(App).use(Quasar, quasarUserOptions).use(router).use(Quasar, quasarUserOptions).mount('#app')
